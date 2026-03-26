e61f337017db8100768a3e81b8a683db5e9c1182
Alumno <Alumno@PC48>
2026-03-26 10:20:32 -0300

    Agregar archivos de proyecto.

---
+++ PryEDAgorioL/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryEDAgorioL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void datosDelDesarrolladorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmDatosdelDesarrollador v = new FrmDatosdelDesarrollador();
+            v.ShowDialog();
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void coloresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmColores ven = new frmColores();
+            ven.ShowDialog();   
+        }
+
+        private void mesesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmMeses vent = new frmMeses();
+            vent.ShowDialog();
+
+        }
+    }
+}
---
+++ PryEDAgorioL/FrmDatosdelDesarrollador.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PryEDAgorioL
+{
+    partial class FrmDatosdelDesarrollador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Location = new System.Drawing.Point(35, 44);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(120, 25);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre: Lautaro Agorio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DNI: 45348947";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(35, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 88);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Datos: Estuadiante de segundo año de la carrera de analista en sistemas de la IES" +
+    ", actualmente trabajando en una startup llamada GURPI";
+            // 
+            // FrmDatosdelDesarrollador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "FrmDatosdelDesarrollador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FrmDatosdelDesarrollador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
