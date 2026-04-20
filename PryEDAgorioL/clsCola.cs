ee98edaadce052becc03d55b8af3de67725c4a3b
guadamolina06 <guadaaalupemolinaaa@gmail.com>
2026-04-20 14:01:57 -0300

    Modificaciones lunes 20/4

---
+++ PryEDAgorioL/clsCola.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace PryEDAgorioL
+{
+    internal class clsCola
+    {
+        private clsNodo pri;
+        private clsNodo ult;
+
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+        public clsNodo Ultimo
+        {
+            get { return ult; }
+            set { ult = value; }
+        }
+
+        public void Agrega (clsNodo nuevo)
+        { 
+                if (Primero==null)
+                {
+                Primero = nuevo;
+                    Ultimo = nuevo;
+                }
+                else
+                {
+                    Ultimo.Siguiente = nuevo;
+                    Ultimo = nuevo;
+                }
+        }
+
+        public void Eliminar()
+        {
+            if (Primero == Ultimo)
+            {
+                Primero = null;
+                Ultimo = null;
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
+            }
+        }
+
+        public void Recorrer(DataGridView Grilla)
+        {
+           clsNodo Aux = Primero;
+            Grilla.Rows.Clear();
+            while (Aux != null)
+            {
+                Grilla.Rows.Add(Aux.Codigo, Aux.Nombre, Aux.Tramite);
+                Aux = Aux.Siguiente;
+            }
+        }
+        public void Recorrer(ListBox Lista)
+        {
+            clsNodo Aux = Primero;
+            Lista.Items.Clear();
+            while (Aux != null)
+            {
+                Lista.Items.Add(Aux.Codigo);
+                Aux = Aux.Siguiente;
+            }
+        }
+        public void Recorrer(ComboBox Combo)
+        {
+            clsNodo Aux = Primero;
+            Combo.Items.Clear();
+            while (Aux != null)
+            {
+                Combo.Items.Add(Aux.Nombre);
+                Aux = Aux.Siguiente;
+            }
+        }
+
+
+
+
+
+    }
+}
